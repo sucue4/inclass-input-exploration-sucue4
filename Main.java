7d628b009978f5366b1cba33f5d0d1a9e2b4d7d9
sucue4 <stephencue05@gmail.com>
2025-09-03 15:39:40 -0700

    update

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in); 
 
-    System.out.println("Enter your name: ");
+    System.out.println("Enter your full name: ");
     String name = input.nextLine();
 
     System.out.print("Enter your age: ");
